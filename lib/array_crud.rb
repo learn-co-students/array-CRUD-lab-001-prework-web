15e0155ea4d499a1d19625f4304c92a47448cf85
Amger <albertgertskis@gmail.com>
2015-11-23 19:25:32 -0500

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
 def create_an_empty_array
-  
+  #creates and returns an empty array
+  my_array = []
+  #can also use just []
 end
 
 def create_an_array
-  
+  my_array = ["Hello World!", "Goobye World!", "Dave Johson", "5"]
+  #can also use contents only w/out my_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retreive_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retreive_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retreive_last_element_from_array(array)
-  
+  array[-1]
 end
