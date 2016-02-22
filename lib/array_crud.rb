fd2c01570241651fe1b006aa1d913a8f3706ad03
kiddeath91 <jason@laskodi.com>
2016-02-22 17:43:16 -0500

    Using Arrays Lab

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+[]
 end
 
 def create_an_array
-  
+my_array = ["square", "circle", "triangle", "rectangle"]
+
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array = ["square", "circle", "triangle", "rectangle"], element = "pentagon")
+  array.push << element
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array = ["square", "circle", "triangle", "rectangle"], element = "pentagon")
+  array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(array = ["square", "circle", "triangle", "rectangle"])
+rec = array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(array = ["square", "circle", "triangle", "rectangle"])
+square = array.shift
 end
 
-def retreive_element_from_index(array, index_number)
-  
+def retreive_element_from_index(array = ["square", "circle", "triangle", "rectangle"], index_number = "1" )
+array[index_number]
 end
 
-def retreive_first_element_from_array(array)
-  
+def retreive_first_element_from_array(array = ["square", "circle", "triangle", "rectangle"])
+array[0]
 end
 
-def retreive_last_element_from_array(array)
-  
+def retreive_last_element_from_array(array = ["square", "circle", "triangle", "rectangle"])
+array[-1]
 end
