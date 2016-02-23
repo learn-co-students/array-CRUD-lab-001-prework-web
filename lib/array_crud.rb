40a3dba1829075dac5da1e8ba20ed5ce13ff58f0
TiciaChance <laticia.chance@gmail.com>
2016-02-23 13:19:46 -0500

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,52 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+
+  this_year = ["money", "love", "completion", "better"] 
   
 end
 
 def add_element_to_end_of_array(array, element)
   
+  feeling = ["tired", "blessed"]
+  feeling.push("arrays!")
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
+  artists = ["beyonce", "kendrick", "rihanna", "drake"]
+  artists.unshift("wow")
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+  shows = ["twd", "arrays!"]
+  shows.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  podcasts = ["wow","ar","tal"]
+  podcasts.shift
 end
 
 def retreive_element_from_index(array, index_number)
+
+  array =["hey", "am", "fucking", "awesome"]
+  array[1]
   
 end
 
 def retreive_first_element_from_array(array)
-  
+  arrays = ["wow", "tee", "crushing", "it"]
+  arrays[0]
 end
 
 def retreive_last_element_from_array(array)
+
+  desperado = ["cold","boi", "code", "is", "bomb", "arrays!"]
+  desperado[-1]
   
 end
