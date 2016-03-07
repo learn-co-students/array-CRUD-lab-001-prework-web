92a57ee687934f60379268657db72b992bb281c1
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-03-07 18:42:48 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  famous_cats = ["lil' bub", "grumpy cat", "Maru", "Garfield"]
+  array = ['one', 'two', 'three', 'four']
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,25 +15,25 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
 array.shift
 end
 
-def retreive_element_from_index(array, index_number)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[2]
+def retreive_element_from_index(array, index)
+array[2]
 end
 
 def retreive_first_element_from_array(array)
-array =  ["wow", "I", "am", "really", "learning", "arrays!"]
   array.first
-
 end
 
 def retreive_last_element_from_array(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.last
+array.last
 end
+
+
+
+
