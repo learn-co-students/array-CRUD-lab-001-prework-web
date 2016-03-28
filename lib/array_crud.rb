e92d74885cf9bcf8a8f9acc71a00ece97eab2348
abcheromar <abcheromar@gmail.com>
2016-03-28 01:25:09 -0400

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,20 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
+  ["cow", "ox", "goat", "dude"]
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
+
+
+arrray = ["cow", "ox", "goat", "dude"]
+
+def add_element_to_end_of_array(array, "dudess")
+
+  array << "dudess"
 end
 
 def add_element_to_start_of_array(array, element)
