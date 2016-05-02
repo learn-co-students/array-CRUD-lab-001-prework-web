42966d29cc05f476ef310f80ea5871c52fd90e68
jazzthespazz <jazhc2001@gmail.com>
2016-05-02 14:33:38 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,93 @@
 def create_an_empty_array
-  
+
+
+
+
+
+
+  empty = []
 end
 
 def create_an_array
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  color = ["orange, "black", "lime", "red]
+
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
+
+
+
+
+
+
+
+  color << "yellow"
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
+
+
+
+
+
+
+  color.unshift("tan")
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
+
+
+  color.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
+
+
+
+  color.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
+
+  color[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+
+
+  color[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+
+
+
+  color[-1]
 end
