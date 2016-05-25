ec1f94c468a011430419153bf8c795e4a542f9fe
abidiaz <abigaildiaz@gmail.com>
2016-05-25 12:29:25 -0400

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,36 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
   array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["I", "am", "really", "learning", "arrays!"]
   array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  end_removal = array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  start_removal = array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[index_number]
 end
 
-def retrieve_first_element_from_array(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"] 
-array[0] 
+def retrieve_first_element_from_array(array) 
+  array[0] 
 end
 
-def retrieve_last_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"] 
+def retrieve_last_element_from_array(array) 
   array.last
 end
