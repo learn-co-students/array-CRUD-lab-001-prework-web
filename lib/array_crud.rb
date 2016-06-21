c25cc5403fec84b7283eac904a72323a8d0a949c
kevin ventura <kevcodes@gmail.com>
2016-06-21 10:09:06 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  favorite_food = ["Taco", "Spring Roll", "Orange Chicken", "Steak"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  favorite_food = ["Taco", "Spring Roll", "Orange Chicken", "Steak"]
+  favorite_food.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  favorite_food = ["Taco", "Spring Roll", "Orange Chicken", "Steak"]
+  favorite_food.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  coding_skills = ["loops","int","typing","arrays!"]
+  coding_skills.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  coding_skills = ["wow","loops","int","typing","arrays!"]
+  coding_skills.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  coding_skills = ["wow","int","am","arrays!"]
+  coding_skills [2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  coding_skills = ["wow","int","am","arrays!"]
+  coding_skills.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  coding_skills = ["wow","int","am","arrays!"]
+  coding_skills.last
 end
